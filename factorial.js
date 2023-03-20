dcde183ab38cdba087db3ad872dda9da59b04631
Daniela Irene Garzón Pinto <digarzonp@unal.edu.co>
2023-03-20 10:41:45 -0500

    version 2.0

---
+++ factorial.js
@@ -0,0 +1,13 @@
+function factorial(){
+    let n= parseInt(document.getElementById('n').value);
+    let factorial=1;
+    let i=2
+
+    while(i<=n){
+        factorial=factorial*i
+        i=i+1
+    }
+
+    return factorial;
+
+}
